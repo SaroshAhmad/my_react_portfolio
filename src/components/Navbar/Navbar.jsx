8b18e2344ef04017576f2d1c7004df86c27523df
SaroshAhmad <ahmadsarosh07@gmail.com>
2025-01-13 22:48:31 +0000

    Fix menu close behavior in Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,14 +16,16 @@ const Navbar = () => {
                     alt='menu-button'
                     onClick={() => setMenuOpen(!menuOpen)}
                 />
-                <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}>
+                <ul
+                    className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+                    onClick={() => setMenuOpen(false)}>
                     <li><a href="#about">About</a></li>
                     <li><a href="#experience">Experience</a></li>
                     <li><a href="#projects">Projects</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
             </div>
-        </nav>
+        </nav >
     )
 }
 
